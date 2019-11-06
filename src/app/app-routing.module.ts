8399056e0539d77f0fbeb3c8ecfa02600a1334f1
Yaroslav Mishchenko <yaroslavzn@gmail.com>
2019-11-06 23:42:51 +0200

    start project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {EntryComponent} from './entry/entry.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: EntryComponent,
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
